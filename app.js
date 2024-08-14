c60df9b7f0a6edcf4d864f08b2138d0eadaec4b9
behruzbek18052008 <behruzbekbaratov1@gmail.com>
2024-08-14 20:41:00 +0500

    uzb

---
+++ app.js
@@ -0,0 +1,61 @@
+
+
+// // let ism = prompt('Ism kirit').trim().toLowerCase();
+
+// // console.log   ( ism == 'Abduqodr' ? 'Ha Abduqodr 1.12da oqiydi' :ism == 'Abdugafor ' ? 'Ha abdugafor 1.12da oqiydi' :ism == 'Asrorbek' ? 'Ha asrorbek 1.12da oqiydi' :ism == ' hayotbek ' ? 'ha hayotbek 1.12da oqiydi' :ism == ' navrozbek ' ? 'ha navrozbek 1.12da oqiydi' :ism=='bizda bunay oquvchi yoq')
+
+// let yosh = prompt('yoshingni kirit')
+
+
+// if(yosh >16){
+//         console.log('pasport')
+
+// }else if(>){
+//         console.log('hali yisg')
+// }
+
+
+// // 
+// let son = prompt('son kiriting')
+// let son2 = prompt('son kiriting')
+
+
+// if (son >son2){
+//         console.log(son)
+// } else {
+//         console.log(son2)
+// }
+
+
+// let ismlar = ['Abdigaffor', 'navrozbek','asrorbek ',' halimjon', 'hayotbek','abdiqodr','behruzbek'] 
+ 
+// console.log(ismlar) 
+ 
+// let res =ismlar.join() 
+// console.log(res) 
+// let name = prompt("Ismingizni kiriting"); 
+ 
+// function reverseString(str) { 
+//     return str.split('').reverse().join(''); 
+// } 
+ 
+// let reversedName = reverseString(name); 
+ 
+// console.log(reversedName);
+
+// let ismlar =['zohid','ali','uzb','aska','anfi','asl'];
+
+// console.log(ismlar)
+
+// let a =0
+// while(a< 6){
+//     console.log(ismlar[a] + 'bek');
+//     a++
+// }
+
+// 1 function declarition
+// qoida functionda default qiymat berilganda hammaparametrlarga default qiymat beriladi
+// function bor(name=ism , days,kun){
+    // let gren = 'salom ${name}hayrli${day}'
+// }    
+// console.log(bor)
